001fe42c0183068a36ee47b6188f37ab6328e8c7
Mateus Cabral <47371964+Cabraiz@users.noreply.github.com>
2022-02-28 04:39:28 -0300

    boots

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="containertudo" class="container mt-5">
+        <h1>Inicio</h1>
+    
+        <a id="createbuttonid" routerLink="create" class="btn btn-success">Criar Dados</a>
+        &nbsp;
+        <a id="readbuttonid" routerLink="read" class="btn btn-primary">Ler Dados</a>
+        <router-outlet></router-outlet> 
+      </div>
     </div>
   );
 }
