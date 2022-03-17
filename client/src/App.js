228d7e11e7bf05590935383779e58f725d435fd2
Mateus Cabral <47371964+Cabraiz@users.noreply.github.com>
2022-03-17 04:43:31 -0300

    svgs

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,11 @@ import {ReactComponent as SVG_Logo} from './Images/SVGS/Principal/Nav/SVG_Logo.s
 
 import {ReactComponent as SVG_SmartPhone} from './Images/SVGS/Principal/Itens/SVG_SmartPhone.svg';
 import {ReactComponent as SVG_Computer} from './Images/SVGS/Principal/Itens/SVG_Computer.svg';
+import {ReactComponent as SVG_Tire} from './Images/SVGS/Principal/Itens/SVG_Tire.svg';
+import {ReactComponent as SVG_Xiaomi} from './Images/SVGS/Principal/Itens/SVG_Xiaomi.svg';
+import {ReactComponent as SVG_Eletrodomesticos} from './Images/SVGS/Principal/Itens/SVG_Eletrodomesticos.svg';
+import {ReactComponent as SVG_Moveis} from './Images/SVGS/Principal/Itens/SVG_Moveis.svg';
+import {ReactComponent as SVG_ArCond} from './Images/SVGS/Principal/Itens/SVG_ArCond.svg';
 
 import Gif from './Images/GIFS/FGif.gif';
 
@@ -43,102 +48,104 @@ function App() {
           </Col>
         </Row>
       </Container>
-      <Navbar bg="navbar navbar-custom pt-1 pb-0" variant="dark">
-          <Container  fluid className="p-0">
-            <Nav className="ms-3"> 
-              <SVG_Logo className='logo'/>
-              <Navbar.Brand href="#">
-                <Form.Label className='cabraiz'>Cabraiz
-                  <Form.Label className='cabraiz_2'>React JS
+      <Container fluid className="p-0 sticky-top">
+        <Navbar bg="navbar navbar-custom pt-1 pb-0" variant="dark">
+            <Container  fluid className="p-0">
+              <Nav className="ms-3"> 
+                <SVG_Logo className='logo'/>
+                <Navbar.Brand href="#">
+                  <Form.Label className='cabraiz'>Cabraiz
+                    <Form.Label className='cabraiz_2'>React JS
+                    </Form.Label>
                   </Form.Label>
-                </Form.Label>
-              </Navbar.Brand>
-              <Navbar.Brand href="#"><Form.Label className='register'>®</Form.Label></Navbar.Brand>
-              <Navbar.Toggle aria-controls="navbarScroll" />
-              <Navbar.Collapse id="navbarScroll">
-                <Nav
-                  className="ms-auto my-2 my-lg-0"
-                  style={{ maxHeight: '40px' }}
-                  navbarScroll
-                >
-                <Form className="d-flex ms-4 me-5">
-                  <FormControl 
-                    type="search"
-                    placeholder="Hoje eu quero..."
-                    aria-label="Search"
-                    fontSize="10px"
-                  />
-                  <Button variant="outline-success"></Button>
-                </Form>
-                <NavDropdown title={<Form.Label className="white">Minha conta</Form.Label>} id="navbarScrollingDropdown" renderMenuOnMount={true}> 
-                  <NavDropdown.Item href="#action4">
-                    Fazer <strong>Login</strong>
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Fazer o <strong>Cadastro</strong>
-                  </NavDropdown.Item> 
-                </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Nav>
-            <Nav className="me-3">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-            </Nav>
-          </Container>
-      </Navbar>
-      <Container fluid className="navbar-custom_2">
-        <Col className="d-flex justify-content-between">
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Smartphones
-          </Button>
-          <Button style={{ whiteSpace: 'nowrap' }} className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Pneus & Auto
-          </Button>
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Climatização
-          </Button>
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Xiaomi
-          </Button> 
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_Computer/>
-            </Col>
-            Computadores
-          </Button> 
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Eletrodomésticos
-          </Button>
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Televisores
-          </Button>
-          <Button className="btn-itens" href="#">
-            <Col>
-              <SVG_SmartPhone/>
-            </Col>
-            Móveis
-          </Button>
-        </Col>
+                </Navbar.Brand>
+                <Navbar.Brand href="#"><Form.Label className='register'>®</Form.Label></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                    className="ms-auto my-2 my-lg-0"
+                    style={{ maxHeight: '40px' }}
+                    navbarScroll
+                  >
+                  <Form className="d-flex ms-4 me-5">
+                    <FormControl 
+                      type="search"
+                      placeholder="Hoje eu quero..."
+                      aria-label="Search"
+                      fontSize="10px"
+                    />
+                    <Button variant="outline-success"></Button>
+                  </Form>
+                  <NavDropdown title={<Form.Label className="white">Minha conta</Form.Label>} id="navbarScrollingDropdown" renderMenuOnMount={true}> 
+                    <NavDropdown.Item href="#action4">
+                      Fazer <strong>Login</strong>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Fazer o <strong>Cadastro</strong>
+                    </NavDropdown.Item> 
+                  </NavDropdown>
+                  </Nav>
+                </Navbar.Collapse>
+              </Nav>
+              <Nav className="me-3">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+              </Nav>
+            </Container>
+        </Navbar>
+        <Container fluid className="navbar-custom_2">
+          <Col className="d-flex justify-content-between">
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_SmartPhone/>
+              </Col>
+              Smartphones
+            </Button>
+            <Button style={{ whiteSpace: 'nowrap' }} className="btn-itens" href="#">
+              <Col>
+                <SVG_Tire/>
+              </Col>
+              Pneus & Auto
+            </Button>
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_ArCond/>
+              </Col>
+              Climatização
+            </Button>
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_Xiaomi/>
+              </Col>
+              Xiaomi
+            </Button> 
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_Computer/>
+              </Col>
+              Computadores
+            </Button> 
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_Eletrodomesticos/>
+              </Col>
+              Eletrodomésticos
+            </Button>
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_SmartPhone/>
+              </Col>
+              Televisores
+            </Button>
+            <Button className="btn-itens" href="#">
+              <Col>
+                <SVG_Moveis/>
+              </Col>
+              Móveis
+            </Button>
+          </Col>
+        </Container>
       </Container>
       <Container fluid className="p-0">
         <Carousel variant="dark" className="sizeCarnival">
@@ -182,6 +189,26 @@ function App() {
               </Col>
           </Carousel.Item>
         </Carousel>
+      </Container>
+      <Container fluid className="navbar-custom_3">
+        <Col className="d-flex justify-content-between pt-4 pb-4">
+          <Col style={{marginLeft:'30px'}}>
+            <Row>Frete Gratis</Row>
+            <Row>Todo Brasil</Row>
+          </Col>
+          <Col>
+            <Row>Parcele</Row>
+            <Row>Em Ate 12x</Row>
+          </Col>
+          <Col>
+            <Row>Sua Compra</Row>
+            <Row>100% Garantida</Row>
+          </Col>
+          <Col>
+            <Row>Feito Para</Row>
+            <Row>Facilitar Sua Vida</Row>
+          </Col>
+        </Col>
       </Container>
       <Container  className="container mt-5 zp">
           <Router>
